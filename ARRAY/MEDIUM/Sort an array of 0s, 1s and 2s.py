f45e5b077c12a106760749fefd492923b7b6f415
Bhavesh S Amborkar <BHAVESHVIRAT06@GMAIL.COM>
2024-02-14 19:50:31 +0530

    Update Sort an array of 0s, 1s and 2s.py

--- ARRAY/MEDIUM/Sort an array of 0s, 1s and 2s.py
+++ ARRAY/MEDIUM/Sort an array of 0s, 1s and 2s.py
@@ -1,3 +1,6 @@
+
+
+
 def dnf(list1):
   low=0
   mid=0
@@ -18,9 +21,21 @@ def dnf(list1):
   return list1
 
 
+
+
+
+ 
 if __name__=="__main__":
   list1=[0,2,1,0,1,0,2,0,2,1,0,2]
   print("After sorting: ",dnf(list1))
 
+
+
+
+# left to low =0, right to high =2, low to mid-1= 1
+#  0 to low -1 = 0, low to mid - 1= 1, high +1 to n-1 = 2 
+# [0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 2]
+#                  |    |  |
+#                 low high mid
 
 
