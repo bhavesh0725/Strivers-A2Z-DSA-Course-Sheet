e4a544e823774077350f62d12800cbc2e65166fb
Bhavesh S Amborkar <BHAVESHVIRAT06@GMAIL.COM>
2024-02-20 22:53:43 +0530

    Update Find the missing number in an array.py

--- ARRAY/EASY/Find the missing number in an array.py
+++ ARRAY/EASY/Find the missing number in an array.py
@@ -20,3 +20,35 @@ def main():
 if __name__ == '__main__':
     main()
 
+
+
+
+
+
+#approach 2:
+def missingNumber(a, N):
+  xor1 = 0
+  xor2 = 0
+
+  for i in range(N - 1):
+      xor2 = xor2 ^ a[i]  # XOR of array elements
+
+  for i in range(1,N+1):
+    xor1 = xor1 ^ (i)  # XOR up to [1...N-1]
+
+  
+
+  return xor1 ^ xor2  # the missing number
+
+
+def main():
+  N = 5
+  a = [1, 2, 4, 5]
+  ans = missingNumber(a, N)
+  print("The missing number is:", ans)
+
+
+if __name__ == '__main__':
+  main()
+
+
